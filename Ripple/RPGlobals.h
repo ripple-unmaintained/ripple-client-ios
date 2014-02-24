6f9507b54df35c667b4217d36bf403bfc7023a46
Kevin Johnson <kevinejohn@gmail.com>
2014-02-24 11:16:29 -0800

    Updated blobvault URL to fix login error. Invalid SSL certificate

--- Ripple/RPGlobals.h
+++ Ripple/RPGlobals.h
@@ -1,6 +1,6 @@
 
 
-#define GLOBAL_BLOB_VAULT           @"https://blobvault.payward.com"
+#define GLOBAL_BLOB_VAULT           @"https://blobvault.ripple.com"
 #define GLOBAL_RIPPLE_LIB_VERSION   @"ripple-0.7.19-min"
 
 #define GLOBAL_XRP_STRING @"XRP"
