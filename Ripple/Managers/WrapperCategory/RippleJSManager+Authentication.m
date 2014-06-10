3522a94c8c313a0f2f2c49e4f6c2e3a392acd156
Kevin Johnson <kevinejohn@gmail.com>
2014-06-10 11:37:26 -0700

    [FIX] Fixed login crashing on migrated ripple blob accounts

--- Ripple/Managers/WrapperCategory/RippleJSManager+Authentication.m
+++ Ripple/Managers/WrapperCategory/RippleJSManager+Authentication.m
@@ -72,72 +72,81 @@
         [self cancelTimeout];
         
         if (responseObject && ![responseObject isKindOfClass:[NSNull class]] && ((NSData*)responseObject).length > 0) {
-            // Login correct
-            
-            // Save password
-            [SSKeychain setPassword:password forService:SSKEYCHAIN_SERVICE account:username];
             
             NSString * response = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
             NSString * decodedResponse = [response base64DecodedString];
-            NSLog(@"%@: login success", self.class.description);
             
-            NSString * key = [NSString stringWithFormat:@"%d|%@%@",username.length, username,password];
-            //NSLog(@"%@: key: %@", self.class.description, key);
-            
-            // Decrypt
-            [_bridge callHandler:@"sjcl_decrypt" data:@{@"key": key,@"decrypt": decodedResponse} responseCallback:^(id responseData) {
-                if (responseData && ![responseData isKindOfClass:[NSNull class]]) {
-                    // Success
-                    NSLog(@"New Blob: %@", responseData);
-                    RPBlobData * blob = [RPBlobData new];
-                    [blob setDictionary:responseData];
-                    _blobData = blob;
-                    
-                    // Collect contacts
-                    NSArray * contacts = [responseData objectForKey:@"contacts"];
-                    _contacts = [NSMutableArray arrayWithCapacity:contacts.count];
-                    for (NSDictionary * contactDic in contacts) {
-                        RPContact * contact = [RPContact new];
-                        [contact setDictionary:contactDic];
-                        [_contacts  addObject:contact];
+            if (decodedResponse) {
+                // Save password
+                [SSKeychain setPassword:password forService:SSKEYCHAIN_SERVICE account:username];
+                
+                NSLog(@"%@: login success", self.class.description);
+                
+                NSString * key = [NSString stringWithFormat:@"%d|%@%@",username.length, username,password];
+                //NSLog(@"%@: key: %@", self.class.description, key);
+                
+                // Decrypt
+                [_bridge callHandler:@"sjcl_decrypt" data:@{@"key": key,@"decrypt": decodedResponse} responseCallback:^(id responseData) {
+                    if (responseData && ![responseData isKindOfClass:[NSNull class]]) {
+                        // Success
+                        NSLog(@"New Blob: %@", responseData);
+                        RPBlobData * blob = [RPBlobData new];
+                        [blob setDictionary:responseData];
+                        _blobData = blob;
+                        
+                        // Collect contacts
+                        NSArray * contacts = [responseData objectForKey:@"contacts"];
+                        _contacts = [NSMutableArray arrayWithCapacity:contacts.count];
+                        for (NSDictionary * contactDic in contacts) {
+                            RPContact * contact = [RPContact new];
+                            [contact setDictionary:contactDic];
+                            [_contacts  addObject:contact];
+                        }
+                        
+                        NSString * wallet = _blobData.account_id;
+                        
+                        [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationUpdatedContacts object:nil userInfo:nil];
+                        
+                        // Save ripple address
+                        [[NSUserDefaults standardUserDefaults] setObject:wallet forKey:USERDEFAULTS_RIPPLE_KEY];
+                        [[NSUserDefaults standardUserDefaults] setObject:username forKey:USERDEFAULTS_RIPPLE_USERNAME];
+                        [[NSUserDefaults standardUserDefaults] synchronize];
+                        
+                        _isLoggedIn = YES;
+                        
+                        block(nil);
+                        
+                        [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationUserLoggedIn object:nil userInfo:wallet];
+                        /*
+                         Example blob
+                         {
+                         "account_id" = rHQFmb4ZaZLwqfFrNmJwnkizb7yfmkRS96;
+                         contacts =     (
+                         );
+                         "master_seed" = snShK2SuSqw7VjAzGKzT5xc1Qyp4K;
+                         "preferred_issuer" =     {
+                         };
+                         "preferred_second_issuer" =     {
+                         };
+                         }
+                         */
                     }
-                    
-                    NSString * wallet = _blobData.account_id;
-                    
-                    [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationUpdatedContacts object:nil userInfo:nil];
-                    
-                    // Save ripple address
-                    [[NSUserDefaults standardUserDefaults] setObject:wallet forKey:USERDEFAULTS_RIPPLE_KEY];
-                    [[NSUserDefaults standardUserDefaults] setObject:username forKey:USERDEFAULTS_RIPPLE_USERNAME];
-                    [[NSUserDefaults standardUserDefaults] synchronize];
-                    
-                    _isLoggedIn = YES;
-                    
-                    block(nil);
-                    
-                    [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationUserLoggedIn object:nil userInfo:wallet];
-                    /*
-                     Example blob
-                     {
-                     "account_id" = rHQFmb4ZaZLwqfFrNmJwnkizb7yfmkRS96;
-                     contacts =     (
-                     );
-                     "master_seed" = snShK2SuSqw7VjAzGKzT5xc1Qyp4K;
-                     "preferred_issuer" =     {
-                     };
-                     "preferred_second_issuer" =     {
-                     };
-                     }
-                     */
-                }
-                else {
-                    // Failed
-                    NSLog(@"decrypt_blob failed response: %@", responseData);
-                    NSError * error = [NSError errorWithDomain:@"login" code:1 userInfo:@{NSLocalizedDescriptionKey: @"Invalid username or password"}];
-                    [self logout];
-                    block(error);
-                }
-            }];
+                    else {
+                        // Failed
+                        NSLog(@"decrypt_blob failed response: %@", responseData);
+                        NSError * error = [NSError errorWithDomain:@"login" code:1 userInfo:@{NSLocalizedDescriptionKey: @"Invalid username or password"}];
+                        [self logout];
+                        block(error);
+                    }
+                }];
+            }
+            else {
+                // Failed
+                NSLog(@"decrypt_blob failed response");
+                NSError * error = [NSError errorWithDomain:@"login" code:1 userInfo:@{NSLocalizedDescriptionKey: @"Invalid username or password"}];
+                [self logout];
+                block(error);
+            }
         }
         else {
             // Login blobvault failed
